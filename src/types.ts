58c21b8f6057762025ba2edbd89b150c917311cc
Zomer <zomerzxc.19@gmail.com>
2021-08-12 23:46:03 +0800

    Add types for covid data

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,33 @@
 export const zoms = 1;
+export interface ICovid {
+  updated: number;
+  country: string;
+  countryInfo: {
+    _id: number;
+    iso2: string;
+    iso3: string;
+    lat: number;
+    long: number;
+    flag: string;
+  };
+  cases: number;
+  todayCases: number;
+  deaths: number;
+  todayDeaths: number;
+  recovered: number;
+  todayRecovered: number;
+  active: number;
+  critical: number;
+  casesPerOneMillion: number;
+  deathsPerOneMillion: number;
+  tests: number;
+  testsPerOneMillion: number;
+  population: number;
+  continent: string;
+  oneCasePerPeople: number;
+  oneDeathPerPeople: number;
+  oneTestPerPeople: number;
+  activePerOneMillion: number;
+  recoveredPerOneMillion: number;
+  criticalPerOneMillion: number;
+}
