430f03f553bd7b090ccce1866d33405b217a406e
Zomer <zomerzxc.19@gmail.com>
2021-08-12 21:46:57 +0800

    Add nav data

---
+++ src/configs.ts
@@ -0,0 +1,28 @@
+export const API_URL =
+  'https://disease.sh/v3/covid-19/countries/philippines?strict=true';
+
+export const API_URL_REGIONS =
+  'https://covid19-api-philippines.herokuapp.com/api/top-regions';
+
+export const navLinks = [
+  {
+    name: 'Home',
+    url: '#home',
+  },
+  {
+    name: 'Cases',
+    url: '#cases',
+  },
+  {
+    name: 'Worldwide',
+    url: '/worldwide',
+  },
+  {
+    name: 'Symptoms',
+    url: '/symptoms',
+  },
+  {
+    name: 'Preventions',
+    url: '/prevention',
+  },
+];
