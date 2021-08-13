961c1ff377f0792bead0c5de1284ddd1b6a52fb8
Zomer <zomerzxc.19@gmail.com>
2021-08-14 03:04:33 +0800

    Add loading spinner

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyledSpinner } from '../styles/componentStyles/SpinnerStyles';
+
+interface ISpinnerProps {}
+
+const Spinner: React.FC<ISpinnerProps> = ({}) => {
+  return <StyledSpinner></StyledSpinner>;
+};
+
+export default Spinner;
---
+++ src/styles/componentStyles/SpinnerStyles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const StyledSpinner = styled.div`
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  border: 3px solid rgba(255, 255, 255, 0.3);
+  border-radius: 50%;
+  border-top-color: var(--white);
+  animation: spin 1s ease-in-out infinite;
+  -webkit-animation: spin 1s ease-in-out infinite;
+
+  @keyframes spin {
+    to {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+  @-webkit-keyframes spin {
+    to {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+`;
